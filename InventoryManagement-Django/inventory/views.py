b6afc17b6a6305189af1cd785787d8f31e87626c
PREMTEJREDDY <gitdemo@gmail.com>
2025-06-29 07:21:44 +0530

    Initial commit

---
+++ InventoryManagement-Django/inventory/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import (
+    View,
+    CreateView, 
+    UpdateView
+)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from .models import Stock
+from .forms import StockForm
+from django_filters.views import FilterView
+from .filters import StockFilter
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+class StockListView(LoginRequiredMixin,FilterView):
+    filterset_class = StockFilter
+    queryset = Stock.objects.filter(is_deleted=False)
+    template_name = 'inventory.html'
+    paginate_by = 10
+
+@login_required
+def stock_list_view(request):
+    # Your view code for stock list
+    return render(request, 'inventory.html')
+
+
+class StockCreateView(SuccessMessageMixin, CreateView):                                 # createview class to add new stock, mixin used to display message
+    model = Stock                                                                       # setting 'Stock' model as model
+    form_class = StockForm                                                              # setting 'StockForm' form as form
+    template_name = "edit_stock.html"                                                   # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                                          # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been created successfully" 
+    
+                         # displays message when form is submitted
+
+    def get_context_data(self, **kwargs):                                               # used to send additional context
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'New Stock'
+        context["savebtn"] = 'Add to Inventory'
+        return context      
+
+    @login_required
+    def stock_create_view(request):
+    # Your view code for creating stock
+       return render(request, 'edit_stock.html')    
+
+
+class StockUpdateView(SuccessMessageMixin, UpdateView):                                 # updateview class to edit stock, mixin used to display message
+    model = Stock                                                                       # setting 'Stock' model as model
+    form_class = StockForm                                                              # setting 'StockForm' form as form
+    template_name = "edit_stock.html"                                                   # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                                          # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been updated successfully"                             # displays message when form is submitted
+
+    def get_context_data(self, **kwargs):                                               # used to send additional context
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Edit Stock'
+        context["savebtn"] = 'Update Stock'
+        context["delbtn"] = 'Delete Stock'
+        return context
+    
+    @login_required
+    def stock_update_view(request, pk):
+    # Your view code for updating stock
+       return render(request, 'edit_stock.html')
+
+
+class StockDeleteView(View):                                                            # view class to delete stock
+    template_name = "delete_stock.html"                                                 # 'delete_stock.html' used as the template
+    success_message = "Stock has been deleted successfully"                             # displays message when form is submitted
+    
+    def get(self, request, pk):
+        stock = get_object_or_404(Stock, pk=pk)
+        return render(request, self.template_name, {'object' : stock})
+
+    def post(self, request, pk):  
+        stock = get_object_or_404(Stock, pk=pk)
+        stock.is_deleted = True
+        stock.save()                                               
+        messages.success(request, self.success_message)
+        return redirect('inventory')
